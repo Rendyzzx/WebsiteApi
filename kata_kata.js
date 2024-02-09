accd96ba8eb6f9578268a501abede51a9a4fe24c
Rendyzzx <101921601+Rendyzzx@users.noreply.github.com>
2024-02-09 09:31:03 -0800

    Update kata_kata.js

--- kata_kata.js
+++ kata_kata.js
@@ -1,6 +1,7 @@
 var quotes = [
     '"Sejauh mana dia merubah mu? cukup jauh :)." - RendyX',
     '"Aku tidak merubahmu tapi hanya menemani dan membantu masalah mu ^_^." - theona',
-    '"Ketika kita berhenti berharap, yang tersisa hanyalah percaya." - Anonymous'
-    // Tambahkan kutipan-kutipan lainnya di sini
+    '"Ketika kita berhenti berharap, yang tersisa hanyalah percaya." - Anonymous',
+    '"Manusia Hanyalah alat." - ASU'
+    // kamu tau? dijadikan pilihan kedua itu enak juga :)
 ];
