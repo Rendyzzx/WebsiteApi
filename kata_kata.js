2a60bf390b0a552e3089290090db29b0dc2dce35
Rendyzzx <101921601+Rendyzzx@users.noreply.github.com>
2024-02-09 09:22:56 -0800

    Create kata_kata.js

---
+++ kata_kata.js
@@ -0,0 +1,6 @@
+var quotes = [
+    '"Sejauh mana dia merubah mu? cukup jauh :)." - RendyX',
+    '"Aku tidak merubahmu tapi hanya menemani dan membantu masalah mu ^_^." - theona',
+    '"Ketika kita berhenti berharap, yang tersisa hanyalah percaya." - Anonymous'
+    // Tambahkan kutipan-kutipan lainnya di sini
+];
